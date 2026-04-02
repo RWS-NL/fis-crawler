3f50fb7083fcc6a2430c6fcef83c9b6eb6363eaf
Fedor Baart <fedor.baart@rws.nl>
2026-04-02 22:47:24 +0200

    refactor: remove deprecated enrichment aliases

--- fis/graph/enrich.py
+++ fis/graph/enrich.py
@@ -3,8 +3,6 @@
 Re-exports enrichment functions from source-specific modules.
 """
 
-import warnings
-
 # FIS enrichment
 from .enrich_fis import (
     load_fis_node_enrichments,
@@ -21,33 +19,12 @@ from .enrich_euris import (
 )
 
 
-# Backward-compatible aliases
-def load_fis_enrichment_data(*args, **kwargs):
-    warnings.warn(
-        "load_fis_enrichment_data is deprecated, use load_fis_node_enrichments instead",
-        DeprecationWarning,
-        stacklevel=2,
-    )
-    return load_fis_node_enrichments(*args, **kwargs)
-
-
-def build_fis_section_enrichment(*args, **kwargs):
-    warnings.warn(
-        "build_fis_section_enrichment is deprecated, use build_fis_edge_enrichments instead",
-        DeprecationWarning,
-        stacklevel=2,
-    )
-    return build_fis_edge_enrichments(*args, **kwargs)
-
-
 __all__ = [
     # FIS
     "load_fis_node_enrichments",
-    "load_fis_enrichment_data",  # Deprecated
     "match_by_geometry",
     "match_by_route_km",
     "build_fis_edge_enrichments",
-    "build_fis_section_enrichment",  # Deprecated
     "enrich_fis_graph",
     # EURIS
     "load_euris_sailing_speed",
